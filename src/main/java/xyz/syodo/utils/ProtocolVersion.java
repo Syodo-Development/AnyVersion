84c1c7693415fed95440fc39dc2719ac820c1295
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-11-15 13:21:07 +0100

    Update ProtocolVersion.java

--- src/main/java/xyz/syodo/utils/ProtocolVersion.java
+++ src/main/java/xyz/syodo/utils/ProtocolVersion.java
@@ -1,14 +1,17 @@
 package xyz.syodo.utils;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 
 public enum ProtocolVersion {
 
-    MINECRAFT_PE_1_21_0(685, "1.21.0"),
-    MINECRAFT_PE_1_21_2(686, "1.21.2"),
     MINECRAFT_PE_1_21_20(712, "1.21.20"),
     MINECRAFT_PE_1_21_30(729,"1.21.30"),
     MINECRAFT_PE_1_21_40(748, "1.21.40");
+
+    @Getter
+    private static final ProtocolVersion[] versions = values();
 
     private final int PROTOCOL;
     private final String VERSION;
@@ -27,11 +30,11 @@ public enum ProtocolVersion {
     }
 
     public static ProtocolVersion getMin() {
-        return values()[0];
+        return versions[0];
     }
 
     public static ProtocolVersion get(int protocol) {
-        return Arrays.stream(values()).filter(p -> p.protocol() == protocol).findAny().get();
+        return Arrays.stream(versions).filter(p -> p.protocol() == protocol).findAny().get();
     }
 
 }
