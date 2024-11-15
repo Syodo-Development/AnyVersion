4b027d589aef84af551eca152e31ae5b180c4bdf
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-11-15 13:21:29 +0100

    Update ProtocolPlayer.java

--- src/main/java/xyz/syodo/manager/ProtocolPlayer.java
+++ src/main/java/xyz/syodo/manager/ProtocolPlayer.java
@@ -1,6 +1,12 @@
 package xyz.syodo.manager;
 
 import cn.nukkit.network.connection.BedrockSession;
+import xyz.syodo.utils.ProtocolVersion;
 
 public record ProtocolPlayer(BedrockSession player, int protocol) {
+
+    public ProtocolVersion getVersion() {
+        return ProtocolVersion.get(protocol);
+    }
+
 }
